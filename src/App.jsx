4e3dd11d658cf788808c9683f3c800088fa5ef44
ThomasMouliaras <82121115+ThomasMouliaras@users.noreply.github.com>
2023-05-13 15:26:31 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends Component {
+  componentDidMount() {
+    this.loadBlockchainData();
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:5173/");
+    const network = await web3.eth.net.getNetworkType();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { account: "" };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1> Farm Trust</h1>
+        <p>Your account: {this.state.account}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
 }
 
-export default App
+export default App;
