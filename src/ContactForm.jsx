bdd7c50f2ef278b7e1a0550cab94a203ea1bb1b2
ThomasMouliaras <82121115+ThomasMouliaras@users.noreply.github.com>
2023-05-22 15:41:08 +0300

    updated contact form

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,20 +1,82 @@
 import React, { useState } from "react";
 
-const FORM_ENDPOINT = "";
+const FORM_ENDPOINT =
+  "https://public.herotofu.com/v1/f2840fb0-f895-11ed-9eca-1f15a141e038";
 
 const ContactForm = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setTimeout(() => {
-      setSubmitted(true);
-    }, 100);
+  const [status, setStatus] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Anything you need to inject dynamically
+
+    const injectedData = {
+      DYNAMIC_DATA_EXAMPLE: 123,
+    };
+
+    const inputs = e.target.elements;
+
+    const data = {};
+
+    for (let i = 0; i < inputs.length; i++) {
+      if (inputs[i].name) {
+        data[inputs[i].name] = inputs[i].value;
+      }
+    }
+
+    Object.assign(data, injectedData);
+
+    fetch(FORM_ENDPOINT, {
+      method: "POST",
+
+      headers: {
+        Accept: "application/json",
+
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        // It's likely a spam/bot request, so bypass it to validate via captcha
+
+        if (response.status === 422) {
+          Object.keys(injectedData).forEach((key) => {
+            const el = document.createElement("input");
+
+            el.type = "hidden";
+
+            el.name = key;
+
+            el.value = injectedData[key];
+
+            e.target.appendChild(el);
+          });
+
+          e.target.submit();
+
+          throw new Error("Please finish the captcha challenge");
+        }
+
+        if (response.status !== 200) {
+          throw new Error(response.statusText);
+        }
+
+        return response.json();
+      })
+
+      .then(() => setStatus("We'll be in touch soon."))
+
+      .catch((err) => setStatus(err.toString()));
   };
 
-  if (submitted) {
+  if (status) {
     return (
       <>
-        <h2>Thank you!</h2>
-        <div>We'll be in touch soon.</div>
+        <div className="text-2xl">Thank you!</div>
+
+        <div className="text-md">{status}</div>
       </>
     );
   }
